ac33ebce62c6969371ded8d5ee2c2d8168cb077e
terry <terry@diezhi.local>
2024-12-03 22:58:58 +0800

    misc

--- misc/main.dart
+++ misc/main.dart
@@ -1,147 +1,182 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:grpc/grpc.dart';
-//import 'package:audioplayers/audioplayers.dart';
-import 'package:just_audio/just_audio.dart';
 import 'chat.pbgrpc.dart';
-import 'chat.pb.dart';
-import "dart:async";
+import 'package:just_audio/just_audio.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        body: Center(
+          child: SendMessageWidget(),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class SendMessageWidget extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _SendMessageWidgetState createState() => _SendMessageWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _controller = TextEditingController();
-  final TextEditingController _messageController = TextEditingController();
-  String _response = '';
-  late ChatServiceClient _stub;
+class _SendMessageWidgetState extends State<SendMessageWidget> with SingleTickerProviderStateMixin {
   late ClientChannel _channel;
-  late StreamController<ChatMessage> _requestStreamController;
-  late Stream<ChatMessage> _responseStream;
-  late AudioPlayer _audioPlayer;
+  late ChatServiceClient _stub;
+  late StreamController<ChatMessage> _messageController;
+  String _responseMessage = '';
+  late AnimationController _animationController;
+  late Animation<Color?> _colorAnimation;
+  late Animation<double> _fontSizeAnimation;
+  final AudioPlayer _player = AudioPlayer();
+
 
   @override
   void initState() {
     super.initState();
+    _initializeGrpcClient();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    _colorAnimation = ColorTween(
+      begin: Colors.blue,
+      end: Colors.blue,
+    ).animate(_animationController);
+
+    _fontSizeAnimation = Tween<double>(
+      begin: 20,
+      end: 40,
+    ).animate(_animationController);
+  }
+
+  void _initializeGrpcClient() {
     _channel = ClientChannel(
-      '192.168.1.47',
+      '10.10.126.85',
       port: 50051,
       options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
     );
+
     _stub = ChatServiceClient(_channel);
-    _requestStreamController = StreamController<ChatMessage>();
-    _responseStream = _stub.sendMessage(_requestStreamController.stream);
-    _audioPlayer = AudioPlayer();
-  }
+    _messageController = StreamController<ChatMessage>();
+    _startListeningToResponses();
 
-  @override
-  void dispose() {
-    _channel.shutdown();
-    _requestStreamController.close();
-    super.dispose();
+    //发送握手消息
+    _sendHandshakeMessage();
   }
 
 
-  Future<void> _createRoom() async {
+  void _sendHandshakeMessage() {
+    final handshakeMessage = ChatMessage(
+      roomId: '1',
+      userName: 'user1',
+      message: 'Handshake',
+    );
+    _messageController.add(handshakeMessage);
+  }
+
+  void _startListeningToResponses() async {
     try {
-      final response = await _stub.createRoom(CreateRoomRequest()..roomName = _controller.text);
-      setState(() {
-        _response = 'Created Room ID: ${response.roomId}';
-      });
+      final responseStream = _stub.sendMessage(_messageController.stream);
+      await for (var response in responseStream) {
+        setState(() {
+          _responseMessage = response.message;
+           _colorAnimation = ColorTween(
+            begin: const Color.fromARGB(255, 248, 117, 108),
+            end: const Color.fromARGB(255, 141, 3, 3),
+          ).animate(_animationController);
+          _animationController.repeat(reverse: true);
+        });
+        print('Received response: $_responseMessage'); // 调试信息
+        _playAudio();
+      }
     } catch (e) {
       setState(() {
-        _response = 'Caught error: $e';
+        _responseMessage = 'Error: $e';
       });
+      print('Error receiving response: $e'); // 调试信息
     }
   }
 
-  void _sendMessage() {
-    final message = ChatMessage()
-      ..roomId = '1'
-      ..userName = 'user'
-      ..message = _messageController.text;
-      _requestStreamController.add(message);
-      _messageController.clear();
+
+  void _playAudio() async {
+     try {
+      await _player.setAsset('assets/alert_sound.mp3'); // 替换为你的音频文件路径
+      _player.setLoopMode(LoopMode.one);
+      _player.play();
+    } catch (e) {
+      print('Error playing audio: $e');
+    }
   }
 
-  void _playSound() async {
-    await _audioPlayer.setAsset('assets/sounds/fire_alert.mp3');
-    await _audioPlayer.play();
+
+   void _sendMessage() {
+    //final request = ChatMessage()..message = 'Hello, Server!';
+    final request = ChatMessage(
+      roomId: '1',
+      userName: 'user1',
+      message: '发生火情!尽快撤离!',
+    );
+    _messageController.add(request);
+    print('add message to stream: ${request.message}'); // 调试信息
+    print('Current stream state: ${_messageController.hasListener}'); // 检查流是否有监听器
+  }
+
+
+  @override
+  void dispose() {
+    _messageController.close();
+    _channel.shutdown();
+    _animationController.dispose();
+    _player.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _controller,
-              decoration: const InputDecoration(
-                hintText: 'Enter Room Name',
-              ),
-            ),
-            ElevatedButton(
-              onPressed: _createRoom,
-              child: const Text('Create Room'),
-            ),
-            TextField(
-              controller: _messageController,
-              decoration: const InputDecoration(
-                hintText: 'Enter Message',
-              ),
-            ),
-            ElevatedButton(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedBuilder(
+          animation: _colorAnimation,
+          builder: (context, child) {
+            return ElevatedButton(
               onPressed: _sendMessage,
-              child: const Text('Send Message'),
-            ),
-            StreamBuilder<ChatMessage>(
-              stream: _responseStream,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  _playSound();
-                  return Text('Received: ${snapshot.data!.message}');
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                }
-                return const Text('Waiting for messages...');
-              },
-            ),
-            Text(_response),
-          ],
+              style: ElevatedButton.styleFrom(
+                backgroundColor: _colorAnimation.value,
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(100), // 调整按钮的大小
+              ),
+              child: const Text(
+                '发现警情',
+                style: TextStyle(fontSize: 30),
+              ),
+            );
+          },
         ),
-      ),
+        const SizedBox(height: 80),
+        AnimatedBuilder(
+          animation: _fontSizeAnimation,
+          builder: (context, child) {
+            return Text(
+              _responseMessage,
+              style: TextStyle(
+                fontSize: _fontSizeAnimation.value,
+                color: Colors.red,
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
